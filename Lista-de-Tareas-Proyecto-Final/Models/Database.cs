a97edc38580cd4262d28b9232421b6ecd2f0a3ef
mifcVeritasJR <167486636+mifcVeritasJR@users.noreply.github.com>
2024-12-03 21:00:12 -0600

    Agregando SQLite a la base de datos

---
+++ Lista-de-Tareas-Proyecto-Final/Models/Database.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista_de_Tareas_Proyecto_Final.Models
+{
+    internal class Database
+    {
+    }
+}
---
+++ Lista-de-Tareas-Proyecto-Final/Models/TaskItem.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Build.Utilities;
+using SQLite;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace TodoListApp
+{
+    public class Database
+    {
+        private readonly SQLiteAsyncConnection _database;
+
+        public Database(string dbPath)
+        {
+            _database = new SQLiteAsyncConnection(dbPath);
+            _database.CreateTableAsync<TaskItem>().Wait();
+        }
+
+        public Task<List<TaskItem>> GetTasksAsync()
+        {
+            return _database.Table<TaskItem>().ToListAsync();
+        }
+
+        public Task<int> SaveTaskAsync(TaskItem task)
+        {
+            return _database.InsertAsync(task);
+        }
+
+        public Task<int> UpdateTaskAsync(TaskItem task)
+        {
+            return _database.UpdateAsync(task);
+        }
+
+        public Task<int> DeleteTaskAsync(TaskItem task)
+        {
+            return _database.DeleteAsync(task);
+        }
+    }
+}
