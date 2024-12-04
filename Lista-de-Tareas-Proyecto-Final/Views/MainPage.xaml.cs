a43740064ecc8b524df160fa6f4b0cf75763e3fc
CordzB <167384843+CordzB@users.noreply.github.com>
2024-12-04 10:12:04 -0600

    Actualización del diseño: Campo de entrada ahora es responsivo y ocupa el ancho total de la pantalla, con botón 'Save' alineado al lado derecho

--- Lista-de-Tareas-Proyecto-Final/Views/MainPage.xaml.cs
+++ Lista-de-Tareas-Proyecto-Final/Views/MainPage.xaml.cs
@@ -1,25 +1,55 @@
-﻿namespace Lista_de_Tareas_Proyecto_Final
+﻿using System.Collections.ObjectModel;
+
+namespace Lista_de_Tareas_Proyecto_Final
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
+
+        public ObservableCollection<TaskItem> Tasks { get; set; }
 
         public MainPage()
         {
             InitializeComponent();
+
+
+            Tasks = new ObservableCollection<TaskItem>();
+
+
+            TaskList.ItemsSource = Tasks;
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+
+        private void OnSaveClicked(object sender, EventArgs e)
         {
-            count++;
+            if (!string.IsNullOrWhiteSpace(TaskEntry.Text))
+            {
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
+                Tasks.Add(new TaskItem
+                {
+                    Title = TaskEntry.Text,
+                    IsCompleted = false
+                });
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
+
+                TaskEntry.Text = string.Empty;
+            }
+        }
+
+
+        private void OnRemoveClicked(object sender, EventArgs e)
+        {
+            var task = (sender as SwipeItem).BindingContext as TaskItem;
+            if (task != null)
+            {
+                Tasks.Remove(task);
+            }
         }
     }
 
+
+    public class TaskItem
+    {
+        public string Title { get; set; }
+        public bool IsCompleted { get; set; }
+    }
 }
